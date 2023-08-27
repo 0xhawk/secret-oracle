3ad2539a73df73f8ad6222207718739f1f33326b
0xhawk <hawk@horizonx.tech>
2023-08-27 16:27:08 +0400

    Initial commit.

---
+++ src/secret_oracle_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { secret_oracle_backend } from "../../declarations/secret_oracle_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await secret_oracle_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
