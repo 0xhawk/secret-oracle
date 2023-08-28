74dae1bfaf72065730a850961f070b807905c36c
0xhawk <hawk@horizonx.tech>
2023-08-28 19:49:22 +0400

    add owner property

--- Makefile
+++ Makefile
@@ -3,15 +3,6 @@
 install: clean
 	./scripts/install.sh
 
-# .PHONY: init-local
-# init-local: 
-# 	./scripts/init_local_balance.sh
-
-# .PHONY: test
-# test: 
-# 	./test/demo.sh
-# 	./test/deposit.sh
-
 .PHONY: clean
 clean: 
 	dfx stop
--- src/simple_oracle/src/lib.rs
+++ src/simple_oracle/src/lib.rs
@@ -1,5 +1,5 @@
-use ic_cdk::{update, query};
-use candid::{candid_method};
+use ic_cdk::{update, query, init};
+use candid::{candid_method, Principal};
 use std::cell::RefCell;
 use std::collections::HashMap;
 
@@ -8,11 +8,13 @@ pub type Key = Vec<u8>;
 
 thread_local! {
     static STORE: RefCell<HashMap<Key, CipherValue>> = RefCell::new(HashMap::default());
+    static OWNER: RefCell<Principal> = RefCell::new(Principal::anonymous());
 }
 
 #[update]
 #[candid_method(update)]
 pub fn set_val(key: Key, val: CipherValue) {
+    // TODO: validation
     STORE.with(|s| {
         s.borrow_mut().insert(key, val);
     });
@@ -20,9 +22,24 @@ pub fn set_val(key: Key, val: CipherValue) {
 
 #[query]
 #[candid_method(query)]
-pub fn get_val(key: Key) -> CipherValue {
+pub fn get_val(key: Key) -> Option<CipherValue> {
     STORE.with(|s| {
         let store = s.borrow();
-        store.get(&key).unwrap().clone()
+        store.get(&key).cloned()
     })
-} 
+}
+
+#[init]
+fn init() {
+    let caller = ic_cdk::caller();
+    ic_cdk::println!("owner: {}", caller);
+    OWNER.with(|o| {
+        *o.borrow_mut() = caller;
+    });
+}
+
+pub fn owner() -> Principal {
+    OWNER.with(|o| {
+        o.borrow().clone()
+    })
+}
