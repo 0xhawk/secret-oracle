70d44a73cc0606fc1b4607d0ad787e08b24d7c92
0xhawk <hawk@horizonx.tech>
2023-09-04 09:10:43 +0300

    add data_provider

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,6 +10,7 @@ dfx stop && dfx start --background --clean
 
 # deploy all canisters
 dfx canister create system_api --specified-id s55qq-oqaaa-aaaaa-aaakq-cai
+dfx deploy data_provider
 dfx deploy system_api
 dfx deploy simple_oracle
 dfx deploy front
